e157a19a1fabc31444b381448f86f4eff4444fea
responderi <rv.mahti@gmail.com>
2019-09-26 10:43:47 +0300

    5.2

--- src/App.js
+++ src/App.js
@@ -47,6 +47,11 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    document.location.reload()
+  }
+
   const loginForm = () => (
     <div>
       <form onSubmit={handleLogin}>
@@ -85,7 +90,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={() => handleLogout()}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
